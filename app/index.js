1624b69bb407a27a17e6d4540eb5d2e2eb6e70ff
Samandar1418 <nabiyevs967@gmail.com>
2023-11-08 23:06:34 +0500

    finish

---
+++ app/index.js
@@ -0,0 +1,38 @@
+const name = document.getElementById('name');
+const age = document.getElementById('age');
+const email = document.getElementById('email');
+const btn = document.getElementById('btn');
+const table = document.createElement("table");
+table.className = "myTable";
+document.body.appendChild(table);
+
+function list() {
+    table.innerHTML = ""; 
+    let users = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : [];
+    for (let i = 0; i < users.length; i++) {
+        let row = table.insertRow(i);
+        for (let j = 0; j < 3; j++) {
+            let cell = row.insertCell(j);
+            cell.innerHTML = users[i][j];
+        }
+    }
+}
+
+function clearData() {
+    localStorage.removeItem('users');
+    list();
+}
+
+btn.addEventListener('click', function () {
+    let users = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : [];
+    if (name.value && age.value && email.value) {
+        let newUser = [name.value, age.value, email.value];
+        users.push(newUser);
+        localStorage.setItem('users', JSON.stringify(users));
+        list();
+    }
+});
+
+window.onload = function() {
+    list();
+};
